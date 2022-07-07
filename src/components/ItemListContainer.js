0b18999f29b4d3a826cf858bb7530359ca77fb22
Gabriel Cañadas <gcanadas@unsj.edu.ar>
2022-07-06 21:04:06 -0300

    Contador con botón

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ItemCount from './ItemCount';
 
 const ItemListContainer = ({ title }) => {
 
@@ -7,6 +8,7 @@ const ItemListContainer = ({ title }) => {
             <div className='container w-full m-auto h-screen px-4 grid grid-cols-1 place-content-center content-center'>
                 <h1 className='font-sans text-2xl font-bold text-stone-50 py-6'>{title}</h1>
                 <p className='font-sans text-base text-stone-100'> Los mejores juegos y consolas al mejor precio </p>
+                <ItemCount stock={5} initial={1} onAdd={(n) => alert(`Agregados ${n} productos`)}/>
             </div>
         </section>
     );
