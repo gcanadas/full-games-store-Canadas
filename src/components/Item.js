394e33bab4c64973358a91729a76be1c3b98d632
Gabriel Cañadas <gcanadas@unsj.edu.ar>
2022-07-19 02:18:06 -0300

    Corrección de errores

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,13 +5,15 @@ import { Link } from 'react-router-dom';
 const Item = ({ name, type, price, pictureURL, stock, id }) => {
 
   return (
-    <div className=" w-64 h-auto p-6 m-0 bg-white rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all transform duration-500">
-        <img className="h-56" src={pictureURL} alt={name}/>
-        <Link to={`../product/${id}`}>
-          <h3 className="text-2xl font-bold text-gray-700 text-center">{name}</h3>
-        </Link>
-        <p className="text-sm mt-5 text-gray-700">Disponible: {stock}</p>
-        <p className="block text-xl font-semibold text-gray-700 cursor-auto">${price}</p>
+    <div className=" w-full h-auto p-6 m-0 bg-white rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all transform duration-500">
+        <img className="h-56 w-56 object-contain object-center m-auto" src={pictureURL} alt={name}/>
+        <div className='h-20 flex justify-center items-center'>
+          <Link to={`../product/${id}`}>
+            <h3 className="text-2xl font-bold text-gray-700 text-center hover:text-gray-400">{name}</h3>
+          </Link>
+        </div>
+        <p className="text-sm text-gray-700 pl-5">Disponible: {stock}</p>
+        <p className="block text-xl font-semibold text-gray-700 cursor-auto pl-5">${price}</p>
         <ItemCount stock={stock} initial={1} onAdd={(n) => alert(`Agregados ${n} productos`)}/>
     </div>
   )
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,7 +4,7 @@ import Item from './Item';
 const itemList = ({ items }) => {
   return (
     <div className="">
-        <div className="grid flex-wrap items-center justify-center h-full grid-cols-4 gap-y-3 justify-items-center">
+        <div className="grid flex-wrap items-center justify-center h-full grid-cols-4 gap-y-3 gap-x-3 justify-items-center">
             {items.map(({name, type, price, pictureURL, stock, id}) => {
                 return(
                 <Item
